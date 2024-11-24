844e989b102432df10280cd7548ea283d27e02ed
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-11-24 20:03:37 +0300

    add constants

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -3,78 +3,69 @@ from datetime import datetime
 from aiogoogle import Aiogoogle
 
 from app.core.config import settings
-
-FORMAT = "%Y/%m/%d %H:%M:%S"
-ROW_COUNT = 100
-COLUMN_COUNT = 11
+from app.core.constants import (DEFAULT_SHEET_PROPERTIES, FORMAT,
+                                PERMISSIONS_BODY_TEMPLATE, SPREADSHEET_LOCALE,
+                                SPREADSHEET_TITLE_TEMPLATE, TABLE_HEADER,
+                                UPDATE_BODY_TEMPLATE, UPDATERANGE,
+                                VALUE_INPUT_OPTION)
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-
     now_date_time = datetime.now().strftime(FORMAT)
 
     service = await wrapper_services.discover('sheets', 'v4')
 
     spreadsheet_body = {
-        'properties': {'title': f'Отчёт от {now_date_time}',
-                       'locale': 'ru_RU'},
-        'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
-                                   'title': 'Лист1',
-                                   'gridProperties':
-                                       {'rowCount': ROW_COUNT,
-                                        'columnCount': COLUMN_COUNT}}}]
+        'properties': {
+            'title': SPREADSHEET_TITLE_TEMPLATE.format(now_date_time),
+            'locale': SPREADSHEET_LOCALE
+        },
+        'sheets': [DEFAULT_SHEET_PROPERTIES]
     }
 
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    spreadsheetid = response['spreadsheetId']
-    return spreadsheetid
+    spreadsheet_id = response['spreadsheetId']
+    return spreadsheet_id
 
 
-async def set_user_permissions(
-        spreadsheetid: str,
-        wrapper_services: Aiogoogle
-) -> None:
-    permissions_body = {'type': 'user',
-                        'role': 'writer',
-                        'emailAddress': settings.email}
+async def set_user_permissions(spreadsheetid: str,
+                               wrapper_services: Aiogoogle) -> None:
+    permissions_body = PERMISSIONS_BODY_TEMPLATE.copy()
+    permissions_body['emailAddress'] = settings.email
+
     service = await wrapper_services.discover('drive', 'v3')
     await wrapper_services.as_service_account(
         service.permissions.create(
             fileId=spreadsheetid,
             json=permissions_body,
             fields="id"
-        ))
+        )
+    )
 
 
-async def spreadsheets_update_value(
-        spreadsheetid: str,
-        projects: list,
-        wrapper_services: Aiogoogle
-) -> None:
+async def spreadsheets_update_value(spreadsheet_id: str,
+                                    projects: list,
+                                    wrapper_services: Aiogoogle) -> None:
     now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
 
-    table_values = [
-        ['Отчёт от', now_date_time],
-        ['Топ проектов по скорости закрытия'],
-        ['Название проекта', 'Время сбора', 'Описание'],
-        *[[str(project['name']), str(project['delta']),
-           str(project['description'])] for project in projects],
+    TABLE_HEADER[0][1] = now_date_time
+
+    table_values = TABLE_HEADER + [
+        [str(project['name']), str(project['delta']),
+         str(project['description'])] for project in projects
     ]
 
-    update_body = {
-        'majorDimension': 'ROWS',
-        'values': table_values
-    }
+    update_body = UPDATE_BODY_TEMPLATE.copy()
+    update_body['values'] = table_values
 
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
-            range='A1:E30',
-            valueInputOption='USER_ENTERED',
+            spreadsheetId=spreadsheet_id,
+            range=UPDATERANGE,
+            valueInputOption=VALUE_INPUT_OPTION,
             json=update_body
         )
     )
