bbd0f6bee6bdc2090ef88182ed2e29c299f48849
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-11-24 12:24:47 +0300

    add google API

---
+++ app/core/config.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings, EmailStr
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Кошачий благотворительный фонд'
+    description: str = 'Сервис для поддержки котиков!'
+    database_url: str = 'sqlite+aiosqlite:///./qr_kot.db'
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
+    universe_domain: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
---
+++ app/crud/charity_project.py
@@ -0,0 +1,60 @@
+from datetime import timedelta
+from typing import Optional
+
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+from app.models.charity_project import CharityProject
+
+
+class CRUDCharityProject(CRUDBase):
+
+    async def get_project_id_by_name(
+            self,
+            project_name: str,
+            session: AsyncSession,
+    ) -> Optional[int]:
+        db_project_id = await session.execute(
+            select(CharityProject.id).where(
+                CharityProject.name == project_name
+            )
+        )
+        db_project_id = db_project_id.scalars().first()
+        return db_project_id
+
+    async def get_open_projects(self, session: AsyncSession):
+        result = await session.execute(
+            select(self.model).where(
+                self.model.fully_invested.is_(False)
+            )
+        )
+        return result.scalars().all()
+
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession
+    ) -> list[dict]:
+        closed_projects = await session.execute(
+            select(
+                CharityProject.name,
+                CharityProject.description,
+                func.julianday(CharityProject.close_date) - func.julianday(CharityProject.create_date).label('delta')
+            ).where(
+                CharityProject.fully_invested
+            )
+        )
+
+        closed_projects_list = []
+
+        for project in closed_projects:
+            closed_projects_list.append({
+                'name': project.name,
+                'description': project.description,
+                'delta': project.delta
+            })
+
+        return sorted(closed_projects_list, key=lambda x: x['delta'])
+
+
+charity_project_crud = CRUDCharityProject(CharityProject)
